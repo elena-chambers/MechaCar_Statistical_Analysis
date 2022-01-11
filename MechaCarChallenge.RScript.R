fb227390dd02f211c4297accb9bf1b4b91a5e5a7
Elena Chambers <chamb448@umn.edu>
2022-01-10 23:52:18 -0600

    suspension coil analysis

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -2,13 +2,24 @@
 library(dplyr)
 library(tidyverse)
 
-# Import & read data as a dataframe
+# Import & read data 
 mecha_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) 
+mecha_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F) 
 
-# Perform linear regression
+# Perform linear regression on mpg data
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg)
 
-# Determine the p-value and the r-squared value
+# Determine the p-value and the r-squared value for mpg data
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg)) 
 
+# summarize Suspension_Coil.csv file in a table.
+total_summary <- mecha_coil %>% summarize(Mean=mean(PSI),
+                                          Median=median(PSI),
+                                          Variance=var(PSI),
+                                          SD=sd(PSI)) 
 
+# group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil’s PSI column
+lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+                                                                         Median=median(PSI),
+                                                                         Variance=var(PSI),
+                                                                         SD=sd(PSI))
